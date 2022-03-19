8b40b4c40408490f6504edd64485e8a0baf59b7f
Joe Strickland <jstric42@uncc.edu>
2022-03-19 10:41:00 -0400

    Modularized the date functions

--- Meet&MusicProject/controllers/eventController.js
+++ Meet&MusicProject/controllers/eventController.js
@@ -7,8 +7,9 @@
 
 */
 
-//Require the music event model so that you may acess the required data to manipulate and use for the website
+//Require the music event model so that you may acess the required data to manipulate and use for the website. Require conversion functions
 const eventModel = require("../models/musicEvent");
+const dateFormatting = require("../controllers/dateFunctions");
 
 //GET /events musicEvents page --> Render the events page with all the different kinds of events, every topic is rendered correctly
 exports.index = (req, res, next) => {
@@ -106,11 +107,11 @@ exports.getSpecificEvent = (req, res, next) => {
             
             let monthNumber = chosenEvent.date.substring(5,7);
 
-            let formattedMonth = convertNumberToMonth(monthNumber);
+            let formattedMonth = dateFormatting.convertNumberToMonth(monthNumber);
 
             let dayNumber = chosenEvent.date.substring(8,10);
 
-            let dayEnding = convertNumberToPrefix(dayNumber);
+            let dayEnding = dateFormatting.convertNumberToPrefix(dayNumber);
 
             let formattedDate = (`${formattedMonth} ${dayNumber}${dayEnding}, ${formattedYear}`);
             
@@ -119,11 +120,11 @@ exports.getSpecificEvent = (req, res, next) => {
             let endNumberHour = chosenEvent.endTime.substring(0,2);
             let endNumberMinute = chosenEvent.endTime.substring(3,5);
 
-            let formattedStartHour = convertTime(startNumberHour);
-            let formattedEndHour = convertTime(endNumberHour);
+            let formattedStartHour = dateFormatting.convertTime(startNumberHour);
+            let formattedEndHour = dateFormatting.convertTime(endNumberHour);
 
-            let startTimePeriod = convertPeriod(startNumberHour);
-            let endTimePeriod = convertPeriod(endNumberHour);
+            let startTimePeriod = dateFormatting.convertPeriod(startNumberHour);
+            let endTimePeriod = dateFormatting.convertPeriod(endNumberHour);
 
 
             let formattedStartTime = (`${formattedStartHour}:${startNumberMinute} ${startTimePeriod}`);
@@ -239,239 +240,3 @@ exports.deleteEvent = (req, res, next) => {
 
 };
 
-//Conversion Methods. All the mappings from Military Times to normal people times. I hate HTML.
-let convertNumberToMonth = (monthNumber) => {
-    let formattedMonth = "";
-    switch(monthNumber) {
-        case "01":
-            formattedMonth = "January";
-            break;
-        case "02":
-            formattedMonth = "February";
-            break;
-        case "03":
-            formattedMonth = "March";
-            break; 
-        case "04":
-            formattedMonth = "April";
-            break;
-        case "05":
-            formattedMonth = "May";
-            break;
-        case "06":
-            formattedMonth = "June";
-            break;
-        case "07":
-            formattedMonth = "July";
-            break;
-        case "08":
-            formattedMonth = "August";
-            break;
-        case "09":
-            formattedMonth = "September";
-            break;
-        case "10":
-            formattedMonth = "October";
-            break;
-        case "11":
-            formattedMonth = "November";
-            break;
-        case "12":
-            formattedMonth = "December";
-            break;
-        default:
-            formattedMonth = "An error has occurred processing the month."                                     
-    }
-    return formattedMonth;
-}
-
-let convertNumberToPrefix = (dayNumber) => {
-    let dayEnding = "";
-        switch(dayNumber) {
-            case "01":
-                dayEnding = "st";
-                break;
-            case "02":
-                dayEnding = "nd";
-                break;
-            case "03":
-                dayEnding = "rd";
-                break; 
-            case "04":
-                dayEnding = "th";
-                break;
-            case "05":
-                dayEnding = "th";
-                break;
-            case "06":
-                dayEnding = "th";
-                break;
-            case "07":
-                dayEnding = "th";
-                break;
-            case "08":
-                dayEnding = "th";
-                break;
-            case "09":
-                dayEnding = "th";
-                break;
-            case "10":
-                dayEnding = "th";
-                break;
-            case "11":
-                dayEnding = "th";
-                break;
-            case "12":
-                dayEnding = "th";
-                break;
-            case "13":
-                dayEnding = "th";
-                break;
-            case "14":
-                dayEnding = "th";
-                break;
-            case "15":
-                dayEnding = "th";
-                break; 
-            case "16":
-                dayEnding = "th";
-                break;
-            case "17":
-                dayEnding = "th";
-                break;
-            case "18":
-                dayEnding = "th";
-                break;
-            case "19":
-                dayEnding = "th";
-                break;
-            case "20":
-                dayEnding = "th";
-                break;
-            case "21":
-                dayEnding = "st";
-                break;
-            case "22":
-                dayEnding = "nd";
-                break;
-            case "23":
-                dayEnding = "rd";
-                break;
-            case "24":
-                dayEnding = "th";
-                break;
-            case "25":
-                dayEnding = "th";
-                break;
-            case "26":
-                dayEnding = "th";
-                break;
-            case "27":
-                dayEnding = "th";
-                break; 
-            case "28":
-                dayEnding = "th";
-                break;
-            case "29":
-                dayEnding = "th";
-                break;
-            case "30":
-                dayEnding = "th";
-                break;
-            case "31":
-                dayEnding = "st";
-                break;
-            default:
-                dayEnding = "An error has occurred processing the prefix."
-            }
-    return dayEnding;
-}
-
-let convertTime = (milHour) => {
-    let normalNumber = "";
-    switch(milHour) {
-        case "01":
-            normalNumber = "01";
-            break;
-        case "02":
-            normalNumber = "02";
-            break;
-        case "03":
-            normalNumber = "03";
-            break; 
-        case "04":
-            normalNumber = "04";
-            break;
-        case "05":
-            normalNumber = "05";
-            break;
-        case "06":
-            normalNumber = "06";
-            break;
-        case "07":
-            normalNumber = "07";
-            break;
-        case "08":
-            normalNumber = "08";
-            break;
-        case "09":
-            normalNumber = "09";
-            break;
-        case "10":
-            normalNumber = "10";
-            break;
-        case "11":
-            normalNumber = "11";
-            break;
-        case "12":
-            normalNumber = "12";
-            break;
-        case "13":
-            normalNumber = "01";
-            break;
-        case "14":
-            normalNumber = "02";
-            break;
-        case "15":
-            normalNumber = "03";
-            break; 
-        case "16":
-            normalNumber = "04";
-            break;
-        case "17":
-            normalNumber = "05";
-            break;
-        case "18":
-            normalNumber = "06";
-            break;
-        case "19":
-            normalNumber = "07";
-            break;
-        case "20":
-            normalNumber = "08";
-            break;
-        case "21":
-            normalNumber = "09";
-            break;
-        case "22":
-            normalNumber = "10";
-            break;
-        case "23":
-            normalNumber = "11";
-            break;
-        case "00":
-            normalNumber = "12";
-            break;
-    }
-    return normalNumber;
-}
-
-let convertPeriod = (periodTime) => {
-    let returnedPeriod = "";
-    if (periodTime < 12) {
-        returnedPeriod = "AM";
-    } else {
-        returnedPeriod = "PM";
-    }
-    return returnedPeriod;
-}
